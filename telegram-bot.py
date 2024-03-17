9205acc9128148246ff8275b8fcb5518751451b0
natan-matos <natanclimaco@gmail.com>
2024-03-17 13:35:09 -0300

    using telebot lib

--- telegram-bot.py
+++ telegram-bot.py
@@ -5,78 +5,107 @@ from flask import Flask, request, Response
 import requests
 import os
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+import telebot
 
 # constants
 TOKEN = '6826005571:AAGTXt-l02duSH5nDRUzEMj2_gtWbOnXJg8'
 URL = f'https://api.telegram.org/bot{TOKEN}/'
 
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler( commands=['1'] )
+def option1( message ):
+    bot.reply_to( message, 'Option1 Working' )
+
+@bot.message_handler( commands=['2'] )
+def option2( message ):
+    bot.reply_to( message, 'Option2 Working' )
+
+@bot.message_handler( commands=['3'] )
+def option3( message ):
+    bot.reply_to( message, 'Option3 Working' )
+
+def check( message ):
+    return True
+
+@bot.message_handler( func=check )
+def send_message( message ):
+    text = """
+    Choose an Option:
+    /1 Input new values
+    /2 See total expenses this month
+    /3 Exit"""
+    bot.reply_to( message, text )
+
+bot.polling()
+
 #--------------------------------------------------------------------------
-def send_message( chat_id, text, reply_markup=None ):
-    url = f'https://api.telegram.org/bot{TOKEN}/'
-    url = url + f'sendMessage?chat_id={chat_id}'
+# def send_message( chat_id, text, reply_markup=None ):
+#     url = f'https://api.telegram.org/bot{TOKEN}/'
+#     url = url + f'sendMessage?chat_id={chat_id}'
     
-    payload = {'text': text}
-    if reply_markup:
-        payload['reply_markup'] = reply_markup
+#     payload = {'text': text}
+#     if reply_markup:
+#         payload['reply_markup'] = reply_markup
 
-    r = requests.post( url, json=payload )
-    print( f'Staus Code{r.status_code}' )
+#     r = requests.post( url, json=payload )
+#     print( f'Staus Code{r.status_code}' )
 
-    return None
+#     return None
 #------------------------------------------------------------------------------
-def parse_message( message ):
-    chat_id = message['message']['chat']['id']
-    value = message['message']['text']
+# def parse_message( message ):
+#     chat_id = message['message']['chat']['id']
+#     value = message['message']['text']
 
-    value = value.replace( '/', '' )
+#     value = value.replace( '/', '' )
 
-    try:
-        value = float( value )
-    except ValueError:
-        value = 'error'
-    return chat_id, value
+#     try:
+#         value = float( value )
+#     except ValueError:
+#         value = 'error'
+#     return chat_id, value
 #--------------------------------------------------------------------------------
-def handle_command( chat_id, command ):
-    if command == 'start':
-        keyboard = InlineKeyboardMarkup( inline_keyboard=[
-            [InlineKeyboardButton( text='Input new expenses', callback_data='input_expenses')],
-            [InlineKeyboardButton( text='See total expenses for Month', callback_data='see_expenses')],
-            [InlineKeyboardMarkup( text='Exit', callback_data='exit')]
-        ])
-        send_message( chat_id, 'Choose an Option:', reply_markup=keyboard )
-    elif command == 'input_expenses':
-        send_message( chat_id, 'Value added' )
-        pass 
-    elif command == 'see_expenses':
-        send_message( chat_id, 'Total expenses:')
-        pass
-    elif command == 'exit':
-        send_message(chat_id, 'Goodby')
-        pass
+# def handle_command( chat_id, command ):
+#     if command == 'start':
+#         keyboard = InlineKeyboardMarkup( inline_keyboard=[
+#             [InlineKeyboardButton( text='Input new expenses', callback_data='input_expenses')],
+#             [InlineKeyboardButton( text='See total expenses for Month', callback_data='see_expenses')],
+#             [InlineKeyboardMarkup( text='Exit', callback_data='exit')]
+#         ])
+#         send_message( chat_id, 'Choose an Option:', reply_markup=keyboard )
+#     elif command == 'input_expenses':
+#         send_message( chat_id, 'Value added' )
+#         pass 
+#     elif command == 'see_expenses':
+#         send_message( chat_id, 'Total expenses:')
+#         pass
+#     elif command == 'exit':
+#         send_message(chat_id, 'Goodby')
+#         pass
 #-----------------------------------------------------------------------------------------
 # API init
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route( '/', methods=['GET', 'POST'] )
-def index():
-    if request.method == 'POST':
-        message = request.get_json()
+# @app.route( '/', methods=['GET', 'POST'] )
+# def index():
+#     if request.method == 'POST':
+#         message = request.get_json()
 
-        chat_id, value = parse_message( message )
+#         chat_id, value = parse_message( message )
 
-        if value != 'error':
-            #write the value and date in the database
-            send_message( chat_id, 'Correct Value')
-            handle_command( chat_id, value )
-            return Response( 'OK', status=200 )
-        else:
-            send_message( chat_id, 'Invalid Value' )
-            return Response( 'Ok', status=200 )
+#         if value != 'error':
+#             #write the value and date in the database
+#             send_message( chat_id, 'Correct Value')
+#             handle_command( chat_id, value )
+#             return Response( 'OK', status=200 )
+#         else:
+#             send_message( chat_id, 'Invalid Value' )
+#             return Response( 'Ok', status=200 )
     
-    else:
-        return '<h1> Expense Tracker Bot </h1>'
+#     else:
+#         return '<h1> Expense Tracker Bot </h1>'
 
 
-if __name__ == '__main__':
-    port = os.environ.get( 'PORT', 5000)
-    app.run( host='0.0.0.0', port=port)
+# if __name__ == '__main__':
+#     port = os.environ.get( 'PORT', 5000)
+#     app.run( host='0.0.0.0', port=port)
