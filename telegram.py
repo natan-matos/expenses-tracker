01646ebc710037a800e9ec5460d87599f10bd541
natan-matos <natanclimaco@gmail.com>
2024-03-17 11:39:33 -0300

    handle_command function addes

--- telegram.py
+++ telegram.py
@@ -4,38 +4,26 @@ from decimal import Decimal
 from flask import Flask, request, Response
 import requests
 import os
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 
 # constants
 TOKEN = '6826005571:AAGTXt-l02duSH5nDRUzEMj2_gtWbOnXJg8'
 URL = f'https://api.telegram.org/bot{TOKEN}/'
 
-#chat id
-# 627469904
-
-# getMe - Bot info
-# https://api.telegram.org/bot6826005571:AAGTXt-l02duSH5nDRUzEMj2_gtWbOnXJg8/getMe
-
-# getUpdates - recive messages
-# https://api.telegram.org/bot6826005571:AAGTXt-l02duSH5nDRUzEMj2_gtWbOnXJg8/getUpdates
-
-# sendMessage
-# https://api.telegram.org/bot6826005571:AAGTXt-l02duSH5nDRUzEMj2_gtWbOnXJg8/sendMessage?chat_id=627469904&text=I%27m%20working 
-
-# setWebhook
-# https://api.telegram.org/bot6826005571:AAGTXt-l02duSH5nDRUzEMj2_gtWbOnXJg8/setWebhook?url=https://expenses-tracker-tewp.onrender.com
-
-# render link
-# https://expenses-tracker-tewp.onrender.com
-
-def send_message( chat_id, text ):
+#--------------------------------------------------------------------------
+def send_message( chat_id, text, reply_markup=None ):
     url = f'https://api.telegram.org/bot{TOKEN}/'
     url = url + f'sendMessage?chat_id={chat_id}'
+    
+    payload = {'text': text}
+    if reply_markup:
+        payload['reply_markup'] = reply_markup
 
-    r = requests.post( url, json={'text': text} )
+    r = requests.post( url, json=payload )
     print( f'Staus Code{r.status_code}' )
 
     return None
-
+#------------------------------------------------------------------------------
 def parse_message( message ):
     chat_id = message['message']['chat']['id']
     value = message['message']['text']
@@ -47,10 +35,25 @@ def parse_message( message ):
     except ValueError:
         value = 'error'
     return chat_id, value
-
-
-
-
+#--------------------------------------------------------------------------------
+def handle_command( chat_id, command ):
+    if command == 'start':
+        keyboard = InlineKeyboardMarkup( inline_keyboard=[
+            [InlineKeyboardButton( text='Input new expenses', callback_data='input_expenses')],
+            [InlineKeyboardButton( text='See total expenses for Month', callback_data='see_expenses')],
+            [InlineKeyboardMarkup( text='Exit', callback_data='exit')]
+        ])
+        send_message( chat_id, 'Choose an Option:', reply_markup=keyboard )
+    elif command == 'input_expenses':
+        send_message( chat_id, 'Value added' )
+        pass 
+    elif command == 'see_expenses':
+        send_message( chat_id, 'Total expenses:')
+        pass
+    elif command == 'exit':
+        send_message(chat_id, 'Goodby')
+        pass
+#-----------------------------------------------------------------------------------------
 # API init
 app = Flask(__name__)
 
