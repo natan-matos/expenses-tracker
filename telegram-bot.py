d3b2084505b5becb2c530d69482569a17e0398bc
natan-matos <natanclimaco@gmail.com>
2024-03-17 16:55:20 -0300

    AWS credentials addedd

--- telegram-bot.py
+++ telegram-bot.py
@@ -3,6 +3,7 @@ import boto3
 from decimal import Decimal
 import telebot
 import pandas as pd
+import os
 
 # constants
 TOKEN = '6826005571:AAGTXt-l02duSH5nDRUzEMj2_gtWbOnXJg8'
@@ -12,7 +13,14 @@ URL = f'https://api.telegram.org/bot{TOKEN}/'
 bot = telebot.TeleBot(TOKEN)
 
 # Connect to AWS dynamodb
-dynamodb = boto3.resource( 'dynamodb', region_name='us-east-2')
+access_key = os.getenv('AWS_ACCESS_KEY_ID')
+secret_key= os.getenv('AWS_SECRET_ACCESS_KEY')
+
+session = boto3.Session(
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key,
+    )
+dynamodb = session.resource( 'dynamodb', region_name='us-east-2')
 table = dynamodb.Table( 'ExpensesTable' )
 
 # expenses and date lists
@@ -40,24 +48,6 @@ def store_data( expenses, date, tags, table ):
         )
 
 #------------------------------------------------------------------------------
-# @bot.message_handler(commands=['1'])
-# def option1(message):
-#     msg = bot.send_message(message.chat.id, 'Insert the value:')
-#     bot.register_next_step_handler(msg, process_expense_step)
-
-# def process_expense_step(message):
-#     try:
-#         expense = float(message.text)
-#         expenses.append(expense)
-
-#         current_date = datetime.datetime.now().strftime( '%Y-%m-%d' ) #datetime.now().strftime('%d-%m-%Y')
-#         date.append(current_date)
-#         store_data(expenses, date, table)
-
-#         bot.reply_to(message, 'Value Added')
-#     except ValueError:
-#         bot.reply_to(message, 'Value Invalid!')
-
 @bot.message_handler(commands=['1'])
 def option1(message):
     msg = bot.send_message(message.chat.id, 'Insert the value:')
@@ -72,20 +62,6 @@ def process_expense_step(message):
     except ValueError:
         bot.reply_to(message, 'Value Invalid!')
 
-# def process_tag_step(message, expense):
-#     tag = message.text
-#     if tag not in valid_tags:
-#         bot.reply_to(message, 'Invalid tag! Please use one of the following tags: ' + ', '.join(valid_tags))
-#         return
-
-#     valid_tags.append(tag)
-#     expenses.append(expense)
-
-#     current_date = datetime.datetime.now().strftime('%Y-%m-%d')
-#     date.append(current_date)
-#     store_data(expenses, date, valid_tags, table)
-
-#     bot.reply_to(message, 'Value Added')
 def process_tag_step(message, expense):
     tag = message.text
 
@@ -165,73 +141,3 @@ def send_message( message ):
 
 bot.polling()
 
-#--------------------------------------------------------------------------
-# def send_message( chat_id, text, reply_markup=None ):
-#     url = f'https://api.telegram.org/bot{TOKEN}/'
-#     url = url + f'sendMessage?chat_id={chat_id}'
-    
-#     payload = {'text': text}
-#     if reply_markup:
-#         payload['reply_markup'] = reply_markup
-
-#     r = requests.post( url, json=payload )
-#     print( f'Staus Code{r.status_code}' )
-
-#     return None
-#------------------------------------------------------------------------------
-# def parse_message( message ):
-#     chat_id = message['message']['chat']['id']
-#     value = message['message']['text']
-
-#     value = value.replace( '/', '' )
-
-#     try:
-#         value = float( value )
-#     except ValueError:
-#         value = 'error'
-#     return chat_id, value
-#--------------------------------------------------------------------------------
-# def handle_command( chat_id, command ):
-#     if command == 'start':
-#         keyboard = InlineKeyboardMarkup( inline_keyboard=[
-#             [InlineKeyboardButton( text='Input new expenses', callback_data='input_expenses')],
-#             [InlineKeyboardButton( text='See total expenses for Month', callback_data='see_expenses')],
-#             [InlineKeyboardMarkup( text='Exit', callback_data='exit')]
-#         ])
-#         send_message( chat_id, 'Choose an Option:', reply_markup=keyboard )
-#     elif command == 'input_expenses':
-#         send_message( chat_id, 'Value added' )
-#         pass 
-#     elif command == 'see_expenses':
-#         send_message( chat_id, 'Total expenses:')
-#         pass
-#     elif command == 'exit':
-#         send_message(chat_id, 'Goodby')
-#         pass
-#-----------------------------------------------------------------------------------------
-# API init
-# app = Flask(__name__)
-
-# @app.route( '/', methods=['GET', 'POST'] )
-# def index():
-#     if request.method == 'POST':
-#         message = request.get_json()
-
-#         chat_id, value = parse_message( message )
-
-#         if value != 'error':
-#             #write the value and date in the database
-#             send_message( chat_id, 'Correct Value')
-#             handle_command( chat_id, value )
-#             return Response( 'OK', status=200 )
-#         else:
-#             send_message( chat_id, 'Invalid Value' )
-#             return Response( 'Ok', status=200 )
-    
-#     else:
-#         return '<h1> Expense Tracker Bot </h1>'
-
-
-# if __name__ == '__main__':
-#     port = os.environ.get( 'PORT', 5000)
-#     app.run( host='0.0.0.0', port=port)
