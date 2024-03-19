aaa3b94681bbeed34d83d11a075c20cbe6afb6a1
natan-matos <natanclimaco@gmail.com>
2024-03-19 14:28:58 -0300

    testing @app.route

--- telegram-bot.py
+++ telegram-bot.py
@@ -30,7 +30,7 @@ table = dynamodb.Table( 'ExpensesTable' )
 
 user_data = {}
 
-@app.route('/', methods=['POST'])
+@app.route(f'/{TOKEN}', methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
